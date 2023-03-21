499c428c5dcd97d88501cd0b1a38a6c6ef556ce0
aronbencsiknu <aron.bencsik@northumbria.ac.uk>
2023-03-21 19:37:03 +0000

    Reward function heat gain logic updated

--- reward.py
+++ reward.py
@@ -1,45 +1,38 @@
 
 class Reward:
-    def __init__(self, max_temp=25, min_temp=20, crit_max_temp=27, crit_min_temp=23, crit_time=30, max_allowed_temp_change=1):
         self.max_temp = max_temp
         self.min_temp = min_temp
         self.crit_max_temp = crit_max_temp
         self.crit_min_temp = crit_min_temp
-        self.crit_time = crit_time
+        self.max_crit_time = max_crit_time
         self.max_allowed_temp_change = max_allowed_temp_change
-        self.crit_time_actual = 0
-        self.cooldown_time = 0
+        self.current_crit_time = 0
+
+        self.plant_heat_gain = 1
     
-    def update(self, max_temp, min_temp, crit_max_temp, crit_min_temp, crit_time):
+    def update(self, max_temp, min_temp, crit_max_temp, crit_min_temp, max_crit_time):
         self.max_temp = max_temp
         self.min_temp = min_temp
         self.crit_max_temp = crit_max_temp
         self.crit_min_temp = crit_min_temp
-        self.crit_time = crit_time
+        self.max_crit_time = max_crit_time
 
     def calculate_reward(self, indoor_temp, indoor_temp_history, heating):
         temp_midpoint = (self.max_temp + self.min_temp) / 2  # midpoint
 
-        # !!placeholder values!!
         prev_indoor_temp = indoor_temp_history[len(indoor_temp_history) - 2]  # get second last temp value
         temp_change = abs(indoor_temp - prev_indoor_temp)
-
-        if self.crit_min_temp <= indoor_temp <= self.crit_max_temp:
-            self.crit_time_actual += 1
-        else:
-            self.crit_time_actual = 0
-
 
         # Calculate reward for indoor temperature control
         if self.min_temp <= indoor_temp <= self.max_temp:
             r1 = 1.0
-            cooldown_time =+ 1
-        elif self.crit_min_temp <= indoor_temp <= self.crit_max_temp and self.crit_time_actual < self.crit_time:
+            if self.current_crit_time < 0:
+                self.current_crit_time -= self.plant_heat_gain
+
+        elif self.crit_min_temp <= indoor_temp <= self.crit_max_temp and self.current_crit_time < self.max_crit_time:
             r1 = 1.0
-            self.cooldown_time = 0
-        elif self.crit_min_temp <= indoor_temp <= self.crit_max_temp and cooldown_time < (self.crit_time*5):
-            r1 = -abs(indoor_temp - temp_midpoint)
-            cooldown_time = 0
+            self.current_crit_time += self.plant_heat_gain
+
         else:
             r1 = -abs(indoor_temp - temp_midpoint)
 
@@ -49,14 +42,10 @@ class Reward:
         else:
             r2 = 1.0
 
-        if self.max_allowed_temp_change < (temp_change) and self.max_allowed_temp_change < (temp_change):
+        if self.max_allowed_temp_change < temp_change and self.max_allowed_temp_change < temp_change:
             r3 = 1.0
         else:
             r3 = -abs(self.max_allowed_temp_change - temp_change)
-
-        if cooldown_time >= (self.crit_time*5):
-            cooldown_time = 0
-        
 
         # reward weighing
         r1_w = 1.0

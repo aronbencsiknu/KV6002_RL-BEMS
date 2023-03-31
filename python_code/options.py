fc553a4eb5b5bfb3c66b8e77494192300a189059
aronbencsiknu <101459795+aronbencsiknu@users.noreply.github.com>
2023-03-31 12:00:05 +0100

    Update options.py

--- python_code/options.py
+++ python_code/options.py
@@ -44,13 +44,13 @@ class Options(object):
         self.path_to_model_from_root = "trained_models"
 
         # name to save a pre-trained model
-        self.model_name_save = "test01"
+        self.model_name_save = "test_er1p1_var-temp"
 
         # name of the pre-trained model to load for local or GUI demo
-        self.model_name_load = "example_er1p1"
+        self.model_name_load = "example"
 
         #  set wether to log data to Weights&Biases
         self.wandb = False
-        self.wandb_key = "edfb94e4b9dca47c397a343d2829e9af262d9e32"
+        self.wandb_key = "INCLUDE YOUR KEY HERE"
         self.wandb_logging_freq = 100
         

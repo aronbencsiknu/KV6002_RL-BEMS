414608195010a714e6f2bb8775fd9940022febd3
aronbencsiknu <101459795+aronbencsiknu@users.noreply.github.com>
2023-04-01 09:49:37 +0100

    Update options.py

--- python_code/options.py
+++ python_code/options.py
@@ -52,6 +52,6 @@ class Options(object):
 
         #  set wether to log data to Weights&Biases
         self.wandb = False
-        self.wandb_key = "edfb94e4b9dca47c397a343d2829e9af262d9e32"
+        self.wandb_key = "ADD YOUR KEY HERE"
         self.wandb_logging_freq = 200
         
